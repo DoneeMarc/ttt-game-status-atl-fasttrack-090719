ecbef1042b7fb3984bd533fb423ea7885c00babc
Brandon Carter <thebrandonmcarter@gmail.com>
2019-09-08 16:36:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,11 +1,9 @@
-# Helper Method
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
 
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
